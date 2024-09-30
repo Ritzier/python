d631ac90780794c6e7ec33225c3b218a10b5425c
ritzier <ritzierwheat@gmail.com>
2024-10-01 01:58:57 +0800

    THIS CODE SOMETHING WENT WRONG

--- selenium_example/selenium_example/__init__.py
+++ selenium_example/selenium_example/__init__.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
@@ -29,37 +30,72 @@ def login(browser):
     TIMEOUT = 10
 
     try:
-        browser.get(BASE_URL)
+        load_cookies(browser)
+    except FileNotFoundError:
+        print("Cookie not found")
+        try:
+            browser.get(BASE_URL)
 
-        form = WebDriverWait(browser, TIMEOUT).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "el-form"))
-        )
+            form = WebDriverWait(browser, TIMEOUT).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "el-form"))
+            )
 
-        form.find_element(By.NAME, "username").send_keys(USERNAME)
-        form.find_element(By.NAME, "password").send_keys(PASSWORD)
+            form.find_element(By.NAME, "username").send_keys(USERNAME)
+            form.find_element(By.NAME, "password").send_keys(PASSWORD)
 
-        # submit_button = WebDriverWait(browser, TIMEOUT).until(
-        #     EC.element_to_be_clickable(
-        #         (By.CSS_SELECTOR, "button.el-button--primary[type='submit']")
-        #     )
-        # )
-        # submit_button.click()
+            # submit_button = WebDriverWait(browser, TIMEOUT).until(
+            #     EC.element_to_be_clickable(
+            #         (By.CSS_SELECTOR, "button.el-button--primary[type='submit']")
+            #     )
+            # )
+            # submit_button.click()
 
-        # Get current url before login
-        initial_url = browser.current_url
+            # Get current url before login
+            initial_url = browser.current_url
 
-        form.find_element(
-            By.XPATH,
-            "//input[@type='submit' and @class='el-button el-button--primary']",
-        ).click()
+            form.find_element(
+                By.XPATH,
+                "//input[@type='submit' and @class='el-button el-button--primary']",
+            ).click()
 
-        WebDriverWait(browser, TIMEOUT).until(EC.url_changes(initial_url))
-        print("Login successful")
+            WebDriverWait(browser, TIMEOUT).until(EC.url_changes(initial_url))
+            print("Login successful")
 
-    except TimeoutException:
-        print("Login elements not found or page load timeout.")
-    except Exception as e:
-        print(f"An error occurred during login: {e}")
+            print("Store cookies")
+            store_cookies(browser)
+
+        except TimeoutException:
+            print("Login elements not found or page load timeout.")
+        except Exception as e:
+            print(f"An error occurred during login: {e}")
+    finally:
+        try:
+            browser.get(BASE_URL)
+            WebDriverWait(browser, TIMEOUT).until(
+                EC.presence_of_element_located(
+                    (
+                        By.XPATH,
+                        "//button[contains(., 'admin') and contains(@class, 'logout')]",
+                    )
+                )
+            )
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
+def store_cookies(browser, cookie_path="cookies.json"):
+    cookies = browser.get_cookies()
+    for cookie in cookies:
+        cookie.pop("domain", None)
+    with open(cookie_path, "w") as file:
+        json.dump(cookies, file)
+
+
+def load_cookies(browser, cookie_path="cookies.json"):
+    with open(cookie_path, "r") as file:
+        cookies = json.load(file)
+        for cookie in cookies:
+            browser.add_cookie(cookie)
 
 
 def main():
@@ -72,7 +108,6 @@ def main():
     else:
         raise ValueError("unsupported WebDriver")
 
-    # Open the provided URL
     login(driver)
 
     input("Pres Enter to close the browser...")
